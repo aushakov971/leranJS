98ed7d7326cbb20c3b7b574d573c3ea3da91848b
aushakov971 <aushakov971@gmail.com>
2021-08-18 23:15:02 +0300

    Learn JS логічні оператори && , ||

---
+++ js/app.js
@@ -0,0 +1,69 @@
+let a = 6;
+
+// if (a > 9) {
+//   console.log('yes');
+// } else {
+//   console.log('else');
+// }
+
+let button = document.querySelector('button');
+let input = document.querySelector('.age');
+
+// console.log(0 && 2);
+
+console.log(0 || undefined);
+
+
+// console.log(false || '' || 0 || 123);
+
+
+// button.onclick = function () {
+//   let num = +input.value;
+//   if (num >= 16 && num <= 60) {
+//     console.log('welcom');
+//   } else if (num > 60) {
+//     console.log('you are here?');
+//   } else {
+//     console.log('don`t go!!!');
+//   }
+// }
+
+// button.onclick = function () {
+//   let num = +input.value;
+//   if (0 && num) {
+//     console.log("yes");
+//   } else {
+//     console.log('no');
+//   }
+// }
+
+// console.log(1 && " " && 'uined' && 132)
+
+
+// || - search first true або вертає останнє значення
+
+// && - search first false або вертає останнє значення
+
+//if працює тіки з тру
+// console.log(2 && 'trio' && 23 && 'ret' && '' && 1);
+
+// 0 - false
+
+// ''  - false
+
+// undefined- false
+
+// null - false
+
+
+// let num = 15;
+
+// switch (num) {
+//   case 15:
+//     console.log('one more year');
+//     break;
+
+//   case 16:
+//     console.log('123');
+//     break;
+// }
