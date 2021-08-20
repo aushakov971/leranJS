fd16eb4f171ec38f6c2d65ec171e00ab092cd813
aushakov971 <aushakov971@gmail.com>
2021-08-20 22:39:39 +0300

    create logIn

--- js/app.js
+++ js/app.js
@@ -1,67 +1,149 @@
-let a = 6;
+// // let a = 6;
 
-// if (a > 9) {
-//   console.log('yes');
-// } else {
-//   console.log('else');
-// }
+// // if (a > 9) {
+// //   console.log('yes');
+// // } else {
+// //   console.log('else');
+// // }
 
-let button = document.querySelector('button');
-let input = document.querySelector('.age');
+// // let button = document.querySelector('button');
+// // let input = document.querySelector('.age');
 
-// console.log(0 && 2);
-
-
-// console.log(false || '' || 0 || 123);
+// // console.log(0 && 2);
 
 
-// button.onclick = function () {
-//   let num = +input.value;
-//   if (num >= 16 && num <= 60) {
-//     console.log('welcom');
-//   } else if (num > 60) {
-//     console.log('you are here?');
+// // console.log(false || '' || 0 || 123);
+
+
+// // button.onclick = function () {
+// //   let num = +input.value;
+// //   if (num >= 16 && num <= 60) {
+// //     console.log('welcom');
+// //   } else if (num > 60) {
+// //     console.log('you are here?');
+// //   } else {
+// //     console.log('don`t go!!!');
+// //   }
+// // }
+
+// // button.onclick = function () {
+// //   let num = +input.value;
+// //   if (0 && num) {
+// //     console.log("yes");
+// //   } else {
+// //     console.log('no');
+// //   }
+// // }
+
+// // console.log(1 && " " && 'uined' && 132)
+
+
+// // || - search first true або вертає останнє значення
+
+// // && - search first false або вертає останнє значення
+
+// //if працює тіки з тру
+// // console.log(2 && 'trio' && 23 && 'ret' && '' && 1);
+
+// // 0 - false
+
+// // ''  - false
+
+// // undefined- false
+
+// // null - false
+
+
+// // let num = 15;
+
+// // switch (num) {
+// //   case 15:
+// //     console.log('one more year');
+// //     break;
+
+// //   case 16:
+// //     console.log('123');
+// //     break;
+// // }
+
+
+// // let hour = 12;
+// // let minute = 30;
+
+// // if (hour == 12 && minute == 30) {
+// //   console.log( 'The time is 12:30' );
+// // }
+
+
+
+// let button = document.querySelector('.btn');
+// let input = document.querySelector('.customInput');
+// let conntent = document.querySelector('.inputValue');
+
+// let baton2 = document.querySelector('.btnData');
+// let input2 = document.querySelector('.webInput');
+
+// baton2.addEventListener('click', onChange2);
+
+// function onChange2() {
+//   if (input2.value) {
+//     input.value = input2.value
 //   } else {
-//     console.log('don`t go!!!');
+//     alert('close!!!!!!!!')
 //   }
 // }
 
-// button.onclick = function () {
-//   let num = +input.value;
-//   if (0 && num) {
-//     console.log("yes");
+
+
+// function onChange() {
+//   if (input.value) {
+//     conntent.innerHTML = input.value
 //   } else {
-//     console.log('no');
+//     alert('close!!!')
 //   }
 // }
 
-// console.log(1 && " " && 'uined' && 132)
 
 
-// || - search first true або вертає останнє значення
-
-// && - search first false або вертає останнє значення
-
-//if працює тіки з тру
-// console.log(2 && 'trio' && 23 && 'ret' && '' && 1);
-
-// 0 - false
-
-// ''  - false
-
-// undefined- false
-
-// null - false
+// button.addEventListener('click', onChange);
 
 
-// let num = 15;
 
-// switch (num) {
-//   case 15:
-//     console.log('one more year');
-//     break;
 
-//   case 16:
-//     console.log('123');
-//     break;
-// }
+
+// // '' - false
+// //   0 - false
+// //   1 - true
+// //   null - false
+// //   undefined - false
+// //   ' ' - true
+// //   -1 - true
+// //   {} - true
+// //   [] - true
+
+
+
+let button = document.querySelector('.go');
+let inputLogin = document.querySelector('.logInInput');
+let inputPass = document.querySelector('.passInput');
+let error = document.querySelector('.error');
+
+button.addEventListener('click', logIn);
+
+function logIn() {
+  if (inputLogin.value && inputPass.value) {
+    console.log('logIn');
+    inputLogin.value = '';
+    inputPass.value = '';
+    error.innerHTML = '';
+  } else if (!inputLogin.value || !inputPass.value) {
+    if (!inputLogin.value && !inputPass.value){
+      return error.innerHTML = 'press logg & pass';
+    }
+    if (!inputLogin.value) {
+      error.innerHTML = 'logIn empty'
+    } else {
+      error.innerHTML = 'password empty'
+    }
+  }
+}
